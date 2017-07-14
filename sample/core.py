0c12733ca64c006c2537ca3d024ebfad45d7596b
ohduran <alvaro.duranb@gmail.com>
2017-07-14 23:37:44 +0100

    First attempt to see a list of concepts. With errors.

--- sample/core.py
+++ sample/core.py
@@ -2,4 +2,19 @@
 import json
 import os
 
-data = json.load(open('projects.json', 'r'))
+
+def open_json_as_dict(filepath):
+    """Open JSON file and turn it into a dictionary object."""
+    with open(filepath, 'r') as f:
+        return json.loads(f.read().decode('utf-8'))[0]  # decode not valid.
+
+
+data = open_json_as_dict('sample.json')
+
+triplets = [triplet
+            for text in data['concepts']
+            for triplet in data['concepts'][text]]
+
+
+for i in range(len(triplets)):
+    print triplets[i]['concept']

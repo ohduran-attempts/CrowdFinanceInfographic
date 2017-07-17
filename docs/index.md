13df32548184a4101ae2819d6cbd3690c0982ce5
ohduran <alvaro.duranb@gmail.com>
2017-07-17 18:18:38 +0100

    Add files via upload

--- docs/index.md
+++ docs/index.md
@@ -1,2 +1,5 @@
 # Documentation
 This is a curated document that include information on how to run this program on your computer.
+
+## Issues with Data:
+time intervals, not time points: hard to discuss trending on data intervals instead of data points.
--- sample/core.py
+++ sample/core.py
@@ -1,5 +1,5 @@
-#!crowdfinance/bin/python
 """This file includes the core of the project."""
+
 import json
 import os
 
@@ -7,18 +7,37 @@ import os
 def open_json_as_dict(filepath):
     """Open JSON file and turn it into a dictionary object."""
     with open(filepath, 'r') as f:
-        return json.loads(f.read())[0]  # decode not valid.
+        return json.loads(f.read())
 
 
-data = open_json_as_dict('projects.json')
+def extract_concepts(filepath):
+    """From the dict, extract the concepts."""
+    data = open_json_as_dict(filepath)
+    len_data = len(data)
+    concepts = []
+    for i in xrange(len_data):
+        start_time = data[i]["start_time"]
+        end_time = data[i]["end_time"]
+        try:
+            triplets = [triplet
+                        for text in data[i]['concepts']
+                        for triplet in data[i]['concepts'][text]]
 
-triplets = [triplet
-            for text in data['concepts']
-            for triplet in data['concepts'][text]]
+            for j in range(len(triplets)):
+                try:
+                    concepts.append([str(triplets[j]['concept']),
+                                    start_time,
+                                    end_time])
+                # avoid concepts that can't be converted into string.
+                except UnicodeEncodeError:
+                    pass
+        # if concepts is missing
+        except KeyError:
+            pass
+    return concepts
 
 
-for i in range(len(triplets)):
-    try:
-        print str(triplets[i]['concept'])
-    except UnicodeEncodeError:
-        print i
+def countcampaigns(filepath):
+    """Count lines in a JSON file."""
+    data = open_json_as_dict(filepath)
+    return len(data)
